b574e34309ffc05d5ba36243d905cf3ba816a241
storylove1993 <135993608+storylove1993@users.noreply.github.com>
2024-08-08 17:38:13 +0000

    main

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x45e6EC41ba6a7424487604b66425493c7e0b35e5'
+export const ProxyAdmin = '0xEE50B73FEbeB0C0035cf514358d7ef204ec2e2A6'
+export const SWTRImplementation = '0x6eE16b055cd1fad1766CAfE7d8D865b4d57F932A'
+    
